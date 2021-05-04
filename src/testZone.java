dd80d56b35131f49724016cbf13e5f9c972ca5e7
Robert Dominguez <dominguez.1st@gmail.com>
2021-05-04 13:49:35 -0600

    6 kyu. Bouncing Balls. Solved.

--- src/testZone.java
+++ src/testZone.java
@@ -600,8 +600,20 @@ public class testZone {
     return null;
   }
 
+  public static int bouncingBall(double h, double bounce, double window) {
+    if (h < 0 || bounce <= 0 || bounce >= 1 || window > h){
+      return -1;
+    }
+    int count = -1;
+    System.out.println(h + " " + bounce + " " + window);
+    for (double i = h; i > window; i *= bounce){
+      count += 2;
+    }
+    return count;
+  }
+
   public static void main(String[] args) {
-    isPerfectPower(4);
+    bouncingBall(3.0, .66, 1.5 );
   }
 }
 
