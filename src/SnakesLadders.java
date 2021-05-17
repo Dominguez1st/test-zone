cdaf4fb65df08b3ca7c10a9cd564304fad710c52
Robert Dominguez <dominguez.1st@gmail.com>
2021-05-17 17:38:15 -0600

    5 kyu. Snakes and Ladders. Incomplete.

--- src/SnakesLadders.java
+++ src/SnakesLadders.java
@@ -1,48 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SnakesLadders {
+  private int[] playerPlacement;
+  private boolean victory;
+  private int turn;
+
   public SnakesLadders() {
-    boolean victory = false;
-    String currentPlayer = "";
-    int playerOnePlacement = 0;
-    int playerTwoPlacement = 0;
-    for (int i = 1; !victory; i++){
-      if (i % 2 != 0){ // Determines whose turn it is.
-        currentPlayer = "Player 1";
-      } else {
-        currentPlayer = "Player 2";
-      }
-      int die1 = ThreadLocalRandom.current().nextInt(1,6 + 1);
-      int die2 = ThreadLocalRandom.current().nextInt(1,6 + 1);
-      if (currentPlayer.equals("Player 1")){ // Keeps track of player 1's position
-        playerOnePlacement += die1 + die2;
-        System.out.println(currentPlayer+" rolled a "+die1+" and a "+die2+"."+currentPlayer+" is on square "+playerOnePlacement+"." );
-      } else { // Keeps track of player 2's position
-        playerTwoPlacement += die1 + die2;
-        System.out.println(currentPlayer+" rolled a "+die1+" and a "+die2+"."+currentPlayer+" is on square "+playerTwoPlacement+"." );
-      }
-      if (playerOnePlacement > 100){ // Sets player 1 back if they went over 100
-        playerOnePlacement = 100 - (playerOnePlacement - 100);
-        System.out.println(currentPlayer+" went over 100 and moved back to "+playerOnePlacement);
-      }
-      if (playerTwoPlacement > 100){ // Sets player 2 back if they went over 100
-        playerTwoPlacement = 100 - (playerTwoPlacement - 100);
-        System.out.println(currentPlayer+" went over 100 and moved back to "+playerTwoPlacement);
-      }
-      if (playerOnePlacement == 100 || playerTwoPlacement == 100){ // Ends the game.
-        System.out.println(currentPlayer+" Wins!");
-       victory = true;
-      }
-      if (die1 == die2){ // Enables current player to goes again.
-        i--;
-      }
-    }
+    playerPlacement = new int[]{0,0};
+    victory = false;
+    turn = 1;
+    Map<Integer, Integer> snakes = new HashMap<>();
+    snakes.put(16,6);
   }
   public String play(int die1, int die2) {
-    return "";
+    int currentPlayer = turn % 2;
+    String currentName = "Player "+ (currentPlayer + 1);
+
+    playerPlacement[currentPlayer] += die1 + die2;
+    System.out.println(currentName+" rolled a "+die1+" and a "+die2+"."+currentName+" is on square "+playerPlacement[currentPlayer]+"." );
+    if (playerPlacement[currentPlayer] > 100){ // Sets player 1 back if they went over 100
+      playerPlacement[currentPlayer] = 100 - (playerPlacement[currentPlayer] - 100);
+      System.out.println(currentName+" went over 100 and moved back to "+playerPlacement[currentPlayer]);
+    }
+    // map
+    if (playerPlacement[currentPlayer] == 100){ // Ends the game.
+      System.out.println(currentName+" Wins!");
+      victory = true;
+    }
+    if (die1 != die2){ // Enables current player to goes again.
+      turn++;
+    }
+    return currentName+" is on square "+ playerPlacement[currentPlayer];
   }
 
   public static void main(String[] args) {
-    new SnakesLadders();
+    SnakesLadders game = new SnakesLadders();
+
+    while (!game.victory){
+      int die1 = ThreadLocalRandom.current().nextInt(1,6 + 1);
+      int die2 = ThreadLocalRandom.current().nextInt(1,6 + 1);
+      game.play(die1,die2);
+    }
   }
 }
