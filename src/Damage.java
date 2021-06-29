36511cf2511e26ef2e739d7115b07a6cc6af07b4
Robert Dominguez <dominguez.1st@gmail.com>
2021-06-29 17:28:30 -0600

    Personal Project. Second draft finished. Using enums now.

--- src/Damage.java
+++ src/Damage.java
@@ -2,67 +2,65 @@ import java.util.Arrays;
 
 public class Damage {
   public static int [] stuff (int attackPower, int attackTypes){ // Attack types: Red = 1, Blue = 2, Green = 3
-    int[] types = valuesOfTypes(attackTypes);
+    AttackType[] types = valuesOfTypes(attackTypes);
     int[] info = new int[3];
 
-    for (int attack : types) {
-      if (attack == 1){
-        int[] redInfo = redDamage(attackPower);
-        info[0] += redInfo[0];
-        info[1] += redInfo[1];
-        info[2] += redInfo[2];
-      }
-      if (attack == 2){
-        int[] blueInfo = blueDamage(attackPower);
-        info[0] += blueInfo[0];
-        info[1] += blueInfo[1];
-        info[2] += blueInfo[2];
-      }
-      if (attack == 3){
-        int[] greenInfo = greenDamage(attackPower);
-        info[0] += greenInfo[0];
-        info[1] += greenInfo[1];
-        info[2] += greenInfo[2];
-      }
+    for (AttackType type : types) {
+      int[] redInfo = calculateDamage(attackPower, type);
+      info[0] += redInfo[0];
+      info[1] += redInfo[1];
+      info[2] += redInfo[2];
     }
     return info;
   }
 
-  public static int [] valuesOfTypes(int attackTypes){
-    int[] digits = new int[3];
+  public static AttackType[] valuesOfTypes(int attackTypes){
+    AttackType[] digits = new AttackType[3];
     for (int i = digits.length; i > 0;){
-      digits [i - 1] = attackTypes % 10;
+      digits [i - 1] = AttackType.values()[(attackTypes % 10)-1];
       attackTypes /= 10;
       i--;
     }
     return digits;
   }
 
-  public static int[] redDamage (int attackPower){
+  public static int[] calculateDamage (int attackPower, AttackType attackType){
     int[] info = new int[3];
-    info[0] = (int) (attackPower * 1.5);
-    info[1] = (int) (Math.random() * 4) + 1;
-    info[2] = (int) (Math.random() * 6) + 1;
+    info[0] = (int) Math.round(attackPower * attackType.attackModifier * (Math.random()*.2 + .9));
+    info[1] = (int) (Math.random() * attackType.manaGenerated) + 1;
+    info[2] = (int) (Math.random() * attackType.criticalChance) + 1;
     return info;
   }
 
-  public static int[] blueDamage (int attackPower){
-    int[] info = new int[3];
-    info[0] = attackPower;
-    info[1] = (int) (Math.random() * 20) + 1;
-    info[2] = (int) (Math.random() * 6) + 1;
-    return info;
-  }
+  enum AttackType {
+    RED(1.5, 4, 6),
+    BLUE( 1, 20, 6),
+    GREEN( .8, 6, 20);
 
-  public static int[] greenDamage (int attackPower){
-    int[] info = new int[3];
-    info[0] = (int) (attackPower * .8);
-    info[1] = (int) (Math.random() * 6) + 1;
-    info[2] = (int) (Math.random() * 10) + 1;
-    return info;
+    private final double attackModifier;
+    private final double manaGenerated;
+    private final double criticalChance;
+
+    AttackType(double attackModifier, double manaGenerated, double criticalChance) {
+      this.attackModifier = attackModifier;
+      this.manaGenerated = manaGenerated;
+      this.criticalChance = criticalChance;
+    }
+
+    public double getAttackModifier() {
+      return attackModifier;
+    }
+
+    public double getManaGenerated() {
+      return manaGenerated;
+    }
+
+    public double getCriticalChance() {
+      return criticalChance;
+    }
   }
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(stuff(20, 123)));
+    System.out.println(Arrays.toString(stuff(100, 111)));
   }
 }
